fb50bb880fdca588d0bb8e615811117b7d9b030c
Eric Smith <eric@smu.edu>
2017-05-11 17:59:13 -0500

    changed coeff functions

--- m3315_project3_template.py
+++ m3315_project3_template.py
@@ -12,34 +12,30 @@ relace double line *** by multiple lines of code.
 
 import numpy
 from numpy import exp,zeros,arange,absolute,max
-from math import exp,log,sqrt,cos,pi,sin,pi,e
+from math import exp,log,sqrt,cos,pi,sin,pi
 import matplotlib.pyplot as plt
 
 # solution function
 def func(x):
-    y = (2 * exp(1))* x *(exp(-x)) - exp(x)
-    # y = (25/(pi**2))*sin(pi*x) + x #example from class
+    y=***
     return y
 
 # righthand side function
 def rfunc(x):
-    y = -(4 * exp(x))
-    # y = 25 * sin(pi*x) #example from class
+    y=***
     return y
 
 # coefficients of the differential equations
-# A=-1; B=0; C=0 #example from class
-A=1; B=2; C=1
+A=***; B=***; C=***
 
 # the domain [xa,xb]
-# xa=0; xb=1 #example from class
-xa=0; xb=2
+xa=***; xb=***
 
 # boudary conditions
-alpha = func(xa); beta = func(xb)
+alpha = func(***); beta = func(***)
 
 # index for methods: 0:Thomas, 1:Jacobi, 2:Gauss-Sidel, 3: SOR
-imethod=3
+imethod=0
 
 # number of cases, each case has different # of unknowns
 ncase=4
@@ -59,22 +55,17 @@ def LU3315(a,b,c,r):
     l = zeros(n,float)
     u = zeros(n,float)
     z = zeros(n,float)
-
     #Determine L,U factors
-    u[0] = b[0]
-    for k in range(1,n):
-        l[k] = a[k]/u[k-1]
-        u[k] = b[k] - l[k] * c[k-1]
+    ***
+    ***
 
     # Solve Lz = r
-    z[0] = r[0]
-    for k in range(1,n):
-        z[k] = r[k] - l[k] * z[k-1]
+    ***
+    ***
 
     # Solve Uw = z.
-    w[n-1] = z[n-1] / u[n-1]
-    for k in range(n-2, -1, -1):
-        w[k] = (z[k] - (c[k] * w[k+1]))/u[k]
+    ***
+    ***
 
     return w
 
@@ -85,7 +76,6 @@ fig, ax = plt.subplots(2,2,sharex=True, sharey=True)
 for icase in range(ncase):
     n = 2**(icase+2)-1   #number of unknowns
     h = (xb-xa)/(n+1);  #mesh size
-
     wopt = 2/(1+sqrt(1-cos(pi*h)**2)) #optimized omega
 
     # exact value at a fine mesh
@@ -97,9 +87,10 @@ for icase in range(ncase):
 
 
     # matrix entry on tri-dialgonals
-    coA = (A/float(h**2)) - (B/float(2*h))
-    coB = (float(-2*A)/float(h**2)) + C
-    coC = (float(A)/float(h**2)) + (float(B)/float(2*h))
+    coA=***
+    coB=***
+    coC=***
+
 
     # claim the vectors needed
     xh = zeros(n,float) #x-values
@@ -109,35 +100,18 @@ for icase in range(ncase):
 
     #begin
     #assisgn values for xh,yh,r
-
-    for i in range(0,n):
-        xh[i] = xa+((i+1)*h)
-        yh[i] = func(xh[i])
-
+    ***
+    ***
     #end
-
-
-    r[0] = rfunc(xh[0]) - (coA*alpha)
-    r[n-1]= rfunc(xh[n-1]) - (coC*beta)
-
-    for i in range(1,n-1):
-        r[i] = rfunc(xh[i])  # assign values in the middle
-
 
     # vectors needed for direct methods
     if (imethod == 0):
         # Thomas's algorithm
         a=zeros(n,float);b=zeros(n,float);c=zeros(n,float)
         # assign values for a,b,c
-        for i in range(n):
-            b[i] = coB
-        for i in range(1, n):
-            a[i] = coA
-        for i in range(0, n-1):
-            c[i] = coC
-
+        ***
+        ***
         wh = LU3315(a,b,c,r)
-
     else:
         # Iterative Methods
         tol=10**(-8); err=1 # initial error and error tolerance
@@ -148,23 +122,17 @@ for icase in range(ncase):
             icount[icase]=icount[icase]+1
             if (imethod==1):
                 # Jacobi
-                wh[0] = (r[0] - (coC * wh1[1]))/coB
-                for i in range(1, n-1):
-                    wh[i] = (r[i] - coC*wh1[i+1] - coA*wh1[i-1])/coB
-                wh[n-1] = (r[n-1] - (coA * wh1[n-2])) / coB;
+                ***
+                ***
 
             elif (imethod==2):
                 # Gauss-Seidel
-                wh[0] = (r[0] - coC * wh[1]) / coB
-                for i in range(1, n-1):
-                    wh[i] = (r[i] - (coA * wh[i-1]) - (coC*wh[i+1]))/coB
-                wh[n-1] = (r[n-1] - coA * wh[n-2]) / coB
+                ***
+                ***
             else:
                 # SOR
-                wh[0] = (coB*wh[0] + (wopt*r[0]) - (coC*wh[1]) - (coB*wh[0]))/coB
-                for i in range(1,n-1):
-                    wh[i] = (coB*wh[i] + wopt*(r[i] - coA*wh[i-1] - coC*wh[i+1] - coB*wh[i]))/coB
-                wh[n-1] = (coB * wh[n-1] + (wopt * (r[n-1] - (coA * wh[n-2]) - (coB * wh[n-1])))) / coB
+                ***
+                ***
 
             err=max(absolute(wh1-wh))
             wh1=wh.copy()
@@ -190,11 +158,9 @@ for icase in range(ncase):
     for i in range(1,n+1):
         xplot[i] = xh[i-1]
         wplot[i] = wh[i-1]
-    kx=int(icase/2); ky=icase%2
-
+    kx=icase/2; ky=icase%2
     ax[kx,ky].plot(xe,ye,'-b',xplot,wplot,'ro' )
     ax[kx,ky].set_title('h='+str(h))
-
 
 plt.savefig('result.pdf',format='pdf')
 plt.show()
